88d7ce463516b383d948f98a66f36a889763a259
ATMartin <martin.alex.t@gmail.com>
2014-06-20 16:06:25 +0000

    Initial seed - Hello, World.

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var Canvas = require('canvas');
+var app = express();
+
+//ROUTES
+app.get('/solid/:wd/:ht', function(req, res) {
+
+	var wd = parseInt(req.params.wd),
+			ht = parseInt(req.params.ht);
+
+	var canvas = new Canvas(wd, ht);
+	var context = canvas.getContext('2d');
+
+	context.fillStyle = '#eee';
+	context.fillRect(0, 0, wd, ht);
+	
+	var textOut = wd + 'x' + ht;
+	context.fillStyle = '#aaa';
+	context.font = 'bold ' + scaledFont(wd*ht) + 'px Arial';
+
+	context.fillText(textOut, (wd / 2) - (context.measureText(textOut).width / 2), (ht / 2) + (scaledFont(wd*ht)/2));
+
+	var stream = canvas.pngStream();
+
+	res.type("png");
+	stream.pipe(res);
+});
+
+app.get('/clipped/:wd/:ht', function(req, res) {
+
+	//var img = new Canvas.Image();
+	//img.src = 'src';
+	//img.onload = function() { context.drawImage(img, 0, 0); };
+
+});
+
+//HELPER METHODS
+var scaledFont = function(area) {
+	var fontSize;
+	if (area < 1000) { fontSize = 10; }
+	else if (area > 100000) { fontSize = 32; }
+	else { fontSize = 14 }
+	return fontSize;
+};
+
+//EXPRESS SERVER STARTUP
+app.listen(4000, '0.0.0.0');
+console.log("Server running on port 4000.");
